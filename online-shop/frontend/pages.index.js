94a95da86a4e8a910827f7c42d957ffaa21b3b51
Aleksis-ggs <aleksisabolins920@gmail.com>
2025-03-19 16:00:23 +0000

    Update pages.index.js

--- online-shop/frontend/pages.index.js
+++ online-shop/frontend/pages.index.js
@@ -1,1 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/products").then((res) => setProducts(res.data));
+  }, []);
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Interneta veikals</h1>
+      <div className="grid grid-cols-4 gap-6 mt-6">
+        {products.map((product) => (
+          <div key={product._id} className="border p-4">
+            <img src={product.image} alt={product.name} className="h-40 w-full object-cover"/>
+            <h2 className="text-lg">{product.name}</h2>
+            <p className="text-gray-700">{product.price} €</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
